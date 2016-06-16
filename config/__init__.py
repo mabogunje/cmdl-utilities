48187b15f8fc6fbfade677ea1b3fba74d964e5dd
Damola Mabogunje <damola@mabogunje.net>
2016-06-16 11:42:13 +0100

    {Improvement]: Decoupled email template from code - Now uses Markdown

--- config/__init__.py
+++ config/__init__.py
@@ -4,11 +4,12 @@
 @summary: SCT Configuration
 '''
 
-CONFIG = { 'SERVER' : 'smtp.gmail.com',
+CONFIG = { 'SERVER' : 'smtp.gmail.com', # For Gmail use smtp.gmail.com
            'PORT' : 587,
-           'USERNAME': '',
-           'PASSWORD': '',
-           'USER': '',
-           'EMAIL': ''
+           'USERNAME': 'email@gmail.com', # Your email address
+           'PASSWORD': 'Your Password',
+           'USER': 'Your Full Name',
+           'EMAIL': 'you@email.com',
+           'TEMPLATE': 'templates/default.md' # Location of the Markdown template to populate
          };
 
